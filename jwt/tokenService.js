61ccf377bd3476fb5753d9bcdf8c62b99f862cbd
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-12-25 13:12:10 +0100

    flow changed
    Now the flow its all inside post, so every post call have a new generated token,

--- jwt/tokenService.js
+++ jwt/tokenService.js
@@ -1,19 +1,24 @@
-const fs = require('fs');
-process.loadEnvFile() 
+const fs = require("fs");
+process.loadEnvFile();
 
-const { getToken } = require('sf-jwt-token')
-const privateKey = fs.readFileSync('certs/private.pem').toString('utf8')
+const { getToken } = require("sf-jwt-token");
+const privateKey = fs.readFileSync("certs/private.pem").toString("utf8");
 
 // Function to generate a JWT token
-async function generateToken () {
+async function generateToken() {
+  try {
     const token = await getToken({
       iss: process.env.CONSUMER_ID,
       sub: process.env.SALESFORCE_USERNAME,
       aud: process.env.LOGIN_URL,
-      privateKey: privateKey
-    })
-   
-    console.log(token)
-  }
-  
-  module.exports = { generateToken };
+      privateKey: privateKey,
+    });
+    console.log("Token generado:", token);
+    console.log("Token generado:", token.access_token);
+    return { instanceUrl: token.instance_url, accessToken: token.access_token };
+  } catch (error) {console.error("Error al generar el token:", error.message);
+    throw error; 
+    }
+}
+
+module.exports = { generateToken };
