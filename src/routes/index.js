5aa42f3a7e1663d15e7e6c0b3e876a57883d838c
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-12-25 16:44:47 +0100

    changed message for server running

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Backend service for getting data from commuting running'); 
 });
 
 module.exports = router;
